3080fb047e562f1b1e0e60e65b96de8679553bc1
Xavier DUQUESNE <xavier.duquesne@enedis.fr>
2023-07-19 17:28:55 +0200

    refacto of odbcConnectionTables for teradata

--- R/db.R
+++ R/db.R
@@ -106,16 +106,24 @@ setMethod("sqlCreateTable", "Teradata",
   })
 
 setMethod(
-  "dbListTables", "Teradata",
-  function(conn, catalog_name = NULL, schema_name = NULL, table_name = NULL,
-    table_type = NULL, ...) {
-    c(dbGetQuery(conn, "HELP VOLATILE TABLE")[["Table SQL Name"]],
-      odbcConnectionTables(conn,
-        name = table_name,
-        catalog_name = catalog_name,
-        schema_name = schema_name,
-        table_type = table_type)$table_name)
-  })
+  "odbcConnectionTables",
+  c("Teradata", "character"),
+  function(conn, name, catalog_name = NULL, schema_name = NULL, table_type = NULL) {
+
+    table_list_from_schema <- connection_sql_tables(
+      conn@ptr,
+      catalog_name = catalog_name,
+      schema_name = schema_name,
+      table_name = name,
+      table_type = table_type)
+
+    c(
+      dbGetQuery(conn, "HELP VOLATILE TABLE")[["Table SQL Name"]],
+      table_list_from_schema
+    )
+
+  }
+)
 
 # SAP HANA ----------------------------------------------------------------
 
