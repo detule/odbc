037a7161a6476ecc934b3a32e6706846a23f3ae5
detule <ogjoneski@gmail.com>
2024-01-06 18:48:42 +0000

    test oracle pipeline

---
+++ .github/odbc/install-oracle-driver.sh
@@ -0,0 +1,9 @@
+mkdir -p /opt/oracle 
+wget --quiet -O /opt/oracle/instantclient-basic-linux.x64-21.8.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/218000/instantclient-basic-linux.x64-21.8.0.0.0dbru.zip
+wget --quiet -O /opt/oracle/instantclient-odbc-linux.x64-21.8.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/218000/instantclient-odbc-linux.x64-21.8.0.0.0dbru.zip
+cd /opt/oracle/
+unzip instantclient-basic-linux.x64-21.8.0.0.0dbru.zip
+unzip instantclient-odbc-linux.x64-21.8.0.0.0dbru.zip
+rm *.zip
+sh instantclient_21_8/odbc_update_ini.sh / /opt/oracle/instantclient_21_8/
+
