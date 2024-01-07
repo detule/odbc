8e51727d2961941ec85bf2909428c72f45c03c3c
detule <ogjoneski@gmail.com>
2024-01-07 17:49:50 +0000

    fixing7

--- .github/odbc/install-oracle-driver.sh
+++ .github/odbc/install-oracle-driver.sh
@@ -2,8 +2,8 @@ mkdir -p /opt/oracle
 wget --quiet -O /opt/oracle/instantclient-basic-linux.x64-21.8.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/218000/instantclient-basic-linux.x64-21.8.0.0.0dbru.zip
 wget --quiet -O /opt/oracle/instantclient-odbc-linux.x64-21.8.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/218000/instantclient-odbc-linux.x64-21.8.0.0.0dbru.zip
 cd /opt/oracle/
-unzip instantclient-basic-linux.x64-21.8.0.0.0dbru.zip
-unzip instantclient-odbc-linux.x64-21.8.0.0.0dbru.zip
+unzip instantclient-basic-linux.x64-21.12.0.0.0dbru.zip
+unzip instantclient-odbc-linux.x64-21.12.0.0.0dbru.zip
 rm *.zip
 sudo ln -s /opt/oracle/instantclient_21_8/libclntsh.so.21.1 /usr/local/lib/
 sudo ln -s /opt/oracle/instantclient_21_8/libclntshcore.so.21.1 /usr/local/lib/
