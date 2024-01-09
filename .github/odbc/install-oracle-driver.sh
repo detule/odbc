0741945850697b504965248c793e4b6742879570
detule <ogjoneski@gmail.com>
2024-01-09 13:27:57 +0000

    try adding different driver versions2

--- .github/odbc/install-oracle-driver.sh
+++ .github/odbc/install-oracle-driver.sh
@@ -5,7 +5,7 @@ wget --quiet -O /opt/oracle/instantclient-odbc-linux.x64-21.12.0.0.0dbru.zip htt
 # 21.12 OL7 RPM
 wget --quiet -O oracle-instantclient-basic-21.12.0.0.0-1.x86_64.rpm https://download.oracle.com/otn_software/linux/instantclient/2112000/oracle-instantclient-basic-21.12.0.0.0-1.x86_64.rpm
 wget --quiet -O oracle-instantclient-odbc-21.12.0.0.0-1.x86_64.rpm https://download.oracle.com/otn_software/linux/instantclient/2112000/oracle-instantclient-odbc-21.12.0.0.0-1.x86_64.rpm
-# 21.12 OL9 RPM
+# 21.12 OL9 ZIP
 wget --quiet -O /opt/oracle/instantclient-basic-linux.x64-21.12.0.0.0dbru.el9.zip https://download.oracle.com/otn_software/linux/instantclient/2112000/el9/instantclient-basic-linux.x64-21.12.0.0.0dbru.el9.zip
 wget --quiet -O /opt/oracle/instantclient-odbc-linux.x64-21.12.0.0.0dbru.el9.zip https://download.oracle.com/otn_software/linux/instantclient/2112000/el9/instantclient-odbc-linux.x64-21.12.0.0.0dbru.el9.zip
 # 19.21 ZIP
@@ -26,5 +26,5 @@ unzip instantclient-odbc-linux.x64-19.21.0.0.0dbru.zip
 #ldd instantclient_21_12/libsqora.so.21.1
 sudo sh instantclient_21_12/odbc_update_ini.sh / /opt/oracle/instantclient_21_12/
 sudo sh instantclient_19_21/odbc_update_ini.sh / /opt/oracle/instantclient_19_21/
-sudo alien -i oracle-instantclient-basic-21.12.0.0.0-1.x86_64.rpm
-sudo alien -i oracle-instantclient-odbc-21.12.0.0.0-1.x86_64.rpm
+#sudo alien -i oracle-instantclient-basic-21.12.0.0.0-1.x86_64.rpm
+#sudo alien -i oracle-instantclient-odbc-21.12.0.0.0-1.x86_64.rpm
