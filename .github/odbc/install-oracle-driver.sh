e68dcd42e864404f3c45a2d9b13c0d044d75b107
detule <ogjoneski@gmail.com>
2024-01-11 01:53:37 +0000

    candidate

--- .github/odbc/install-oracle-driver.sh
+++ .github/odbc/install-oracle-driver.sh
@@ -1,30 +1,6 @@
 mkdir -p /opt/oracle 
-# 21.12 zip
 wget --quiet -O /opt/oracle/instantclient-basic-linux.x64-21.12.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/2112000/instantclient-basic-linux.x64-21.12.0.0.0dbru.zip
 wget --quiet -O /opt/oracle/instantclient-odbc-linux.x64-21.12.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/2112000/instantclient-odbc-linux.x64-21.12.0.0.0dbru.zip
-# 21.12 OL7 RPM
-wget --quiet -O oracle-instantclient-basic-21.12.0.0.0-1.x86_64.rpm https://download.oracle.com/otn_software/linux/instantclient/2112000/oracle-instantclient-basic-21.12.0.0.0-1.x86_64.rpm
-wget --quiet -O oracle-instantclient-odbc-21.12.0.0.0-1.x86_64.rpm https://download.oracle.com/otn_software/linux/instantclient/2112000/oracle-instantclient-odbc-21.12.0.0.0-1.x86_64.rpm
-# 21.12 OL9 ZIP
-wget --quiet -O /opt/oracle/instantclient-basic-linux.x64-21.12.0.0.0dbru.el9.zip https://download.oracle.com/otn_software/linux/instantclient/2112000/el9/instantclient-basic-linux.x64-21.12.0.0.0dbru.el9.zip
-wget --quiet -O /opt/oracle/instantclient-odbc-linux.x64-21.12.0.0.0dbru.el9.zip https://download.oracle.com/otn_software/linux/instantclient/2112000/el9/instantclient-odbc-linux.x64-21.12.0.0.0dbru.el9.zip
-# 19.21 ZIP
-wget --quiet -O /opt/oracle/instantclient-basic-linux.x64-19.21.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/1921000/instantclient-basic-linux.x64-19.21.0.0.0dbru.zip
-wget --quiet -O /opt/oracle/instantclient-odbc-linux.x64-19.21.0.0.0dbru.zip https://download.oracle.com/otn_software/linux/instantclient/1921000/instantclient-odbc-linux.x64-19.21.0.0.0dbru.zip
-
 cd /opt/oracle/
 unzip instantclient-basic-linux.x64-21.12.0.0.0dbru.zip
 unzip instantclient-odbc-linux.x64-21.12.0.0.0dbru.zip
-
-#unzip instantclient-basic-linux.x64-19.21.0.0.0dbru.zip
-#unzip instantclient-odbc-linux.x64-19.21.0.0.0dbru.zip
-#rm *.zip
-#sudo ln -s /opt/oracle/instantclient_21_12/libclntsh.so.21.1 /usr/local/lib/
-#sudo ln -s /opt/oracle/instantclient_21_12/libclntshcore.so.21.1 /usr/local/lib/
-#sudo ln -s /opt/oracle/instantclient_21_12/libnnz21.so /usr/local/lib
-#sudo ldconfig
-#ldd instantclient_21_12/libsqora.so.21.1
-sudo sh instantclient_21_12/odbc_update_ini.sh / /opt/oracle/instantclient_21_12/
-#sudo sh instantclient_19_21/odbc_update_ini.sh / /opt/oracle/instantclient_19_21/
-#sudo alien -i oracle-instantclient-basic-21.12.0.0.0-1.x86_64.rpm
-#sudo alien -i oracle-instantclient-odbc-21.12.0.0.0-1.x86_64.rpm
--- tests/testthat/test-driver-oracle.R
+++ tests/testthat/test-driver-oracle.R
@@ -6,80 +6,22 @@ test_that("Oracle", {
     name = "PostgreSQL"
   )
 
-  DBItest::test_result(c(
-    "fetch_n_bad", # TODO
-    "fetch_n_good_after_bad", # TODO
-    "fetch_no_return_value", # TODO
-    "get_query_n_bad", # todo
-    "get_query_good_after_bad_n", # todo
-    "get_query_n_zero_rows", # todo
-    "fetch_no_return_value", # TODO
-    "data_raw.*", # cast(1 bytea) is not valid `cannot cast type integer to bytea`
-    "^data_time$", "^data_time_.*", # `time()` function is not valid syntax
-    "^data_timestamp.*", # We explicitly want to set tzone to UTC
-    "data_64_bit_numeric_warning", # TODO
-    "data_64_bit_lossless", # TODO
-    "data_integer", # Fails, unreliably on Windows
-    "send_query_syntax_error", # TODO
-    "get_query_syntax_error", # TODO
-    "send_query_params", # TODO
-    "fetch_n_multi_row_inf", # TODO
-    "get_query_n_multi_row_inf", # TODO
-    "get_query_n_incomplete", # TODO
-    "get_query_params", # TODO
-    "send_statement_params", # TODO
-    "execute_params", # TODO
-    NULL
-  ))
-  DBItest::test_sql(c(
-    "quote_identifier_vectorized", # Can't implement until https://github.com/rstats-db/DBI/issues/71 is closed
-    "quote_identifier_special", # TODO
-    "roundtrip_timestamp", # We explicitly want to set tzone to UTC
-    "roundtrip_time",
-    "roundtrip_raw", # TODO
-    "list_tables",
-    ".*_table_name",
-    "write_table_error", # TODO
-    "unquote_identifier_vectorized", # TODO
-    "create_table_overwrite", # TODO
-    "create_table_error", # TODO
-    "create_temporary_table", # TODO
-    "append_table_.*", # TODO
-    "append_roundtrip_.*", # TODO
-    "append_table_.*", # TODO
-    "roundtrip_64_bit_roundtrip", # TODO
-    "roundtrip_character", # TODO
-    "roundtrip_field_types", # TODO
-    "write_table_append_incompatible",
-    "write_table_row_names_default", # TODO
-    "remove_table_temporary_arg", # TODO
-    "remove_table_missing_succeed", # TODO
-    "remove_table_temporary", # TODO
-    "list_objects_features", # TODO
-    "list_fields_wrong_table", # TODO
-    "list_fields_quoted", # TODO
-    "list_fields_object", # TODO
-    NULL
-  ))
-  DBItest::test_meta(c(
-    "bind_.*", # TODO
-    "has_completed_statement",
-    "get_statement_statement",
-    "column_info_consistent", # TODO
-    "row_count_statement", # TODO
-    "rows_affected_statement", # TODO
-    "rows_affected_query", # TODO
-    "get_info_result", # TODO
-    NULL
-  ))
-  DBItest::test_transaction(c(
-    NULL
-  ))
-  DBItest::test_compliance(c(
-    "compliance", # We are defining additional subclasses for OdbcConnections
-    "reexport", # TODO
-    NULL
-  ))
+  # Long/outstanding issue with batch inserting
+  # date/datetime for Oracle.  See for example
+  # #349
+  test_roundtrip(columns = c("date", "datetime"))
 
-  test_roundtrip()
+  local({
+    # Test custom dbExistsTable implementation for
+    # Oracle
+    con <- DBItest:::connect(DBItest:::get_default_context())
+    dbWriteTable(con, "mtcarstest", mtcars)
+    expect_true(dbExistsTable(con, "mtcarstest"))
+    dbWriteTable(con, "mtcars_test", mtcars)
+    expect_true(dbExistsTable(con, "mtcars_test"))
+    on.exit({
+      dbExecute(con, "DROP TABLE mtcarstest")
+      dbExecute(con, "DROP TABLE mtcars_test")
+    })
+  })
 })
